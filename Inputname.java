dfa20bac61710c2d8b4f6b12ea309085d6ddf2ac
Ashwini Kumar <91528679+ashwinikumar18@users.noreply.github.com>
2023-06-03 01:48:09 +0530

    4. Input name

---
+++ Inputname.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Inputname {
+ public static void main(String[] args){
+    Scanner xy = new Scanner(System.in);
+    String name = xy.next();
+    System.out.println(name);
+ }
+}
