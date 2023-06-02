94d69b8222e6461c4db1ad2418d4252d866845a5
Ashwini Kumar <91528679+ashwinikumar18@users.noreply.github.com>
2023-06-03 01:46:48 +0530

    3.Print Sum

---
+++ Sum.java
@@ -0,0 +1,11 @@
+public class Sum {
+    public static void main(String args[]){
+        int a=10;
+        int b=25;
+        int sum = a+b;
+        int diff= a-b;
+        System.out.println(sum);
+        System.out.println(diff);
+    }
+    
+}
