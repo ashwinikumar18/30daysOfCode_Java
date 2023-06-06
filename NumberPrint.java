8c42ca24aadb49efe1dbf23e0b53b9bd9cb45dcc
Ashwini Kumar <91528679+ashwinikumar18@users.noreply.github.com>
2023-06-06 23:06:46 +0530

    10. Print number 0-10

---
+++ NumberPrint.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class NumberPrint{
+    public static void main(String args[]){
+        for(int i=0; i<11; i++){
+            System.out.println(i);
+        }
+    }
+}
