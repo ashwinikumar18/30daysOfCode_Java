8c14ea10667c9401af6cf7443ee5830dfce42a66
Ashwini Kumar <91528679+ashwinikumar18@users.noreply.github.com>
2023-06-08 10:47:55 +0530

    11. Number

---
+++ Number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Number{
+    public static void main(String args[]){
+        int i = 0;
+        while( i < 21 )
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
