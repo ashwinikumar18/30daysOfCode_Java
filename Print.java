44b6e89e1b8cd0bdbd87ac1de4a793458b0b0459
Ashwini Kumar <91528679+ashwinikumar18@users.noreply.github.com>
2023-06-08 10:51:04 +0530

    12. Print 1-10

---
+++ Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Print {
+ public static void main(String args[]){
+    int i = 0;
+    do{
+        System.out.println(i);
+        i = i+1;
+    }while( i < 11);
+ }  
+}
