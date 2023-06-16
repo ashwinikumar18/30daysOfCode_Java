3b74db433f8a7df96538edc6809c0b5a987a76d4
Ashwini Kumar <91528679+ashwinikumar18@users.noreply.github.com>
2023-06-16 18:22:02 +0530

    24. Store marks in array

---
+++ Array.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Array {
+    public static void main(String args[]){
+       int[] marks = new int[3];
+       marks[0] =  95; //english
+       marks[1] =  97; //phy
+       marks[2] =  98; //che
+       System.out.println(marks[0]);
+       System.out.println(marks[1]);
+       System.out.println(marks[2]);
+    }
+}
