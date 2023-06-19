1820619fb98a9031e49c109d8f68ab68052b33d1
Ashwini Kumar <91528679+ashwinikumar18@users.noreply.github.com>
2023-06-19 20:45:59 +0530

    30. Measure the length of string

---
+++ StringLength.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class StringLength {
+    public static void main(String args[]){
+        String FirstName = "Ashwini";
+        String LastName  = "Kumar";
+        String FullName  = FirstName + LastName;
+
+        System.out.println(FullName.length());
+
+    }
+}
+// Length of the String
