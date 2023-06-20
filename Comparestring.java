ef0b554938440987809dc6732daca115a754187d
Ashwini Kumar <91528679+ashwinikumar18@users.noreply.github.com>
2023-06-20 16:39:04 +0530

    32. Comparing the Strings

---
+++ Comparestring.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Comparestring {
+    public static void main(String args[]){
+        String name1 = "Tony";
+        String name2 = "Sony";
+
+        if(name1.compareTo(name2) == 0){
+            System.out.println("String are equal.");
+        }
+        else{
+            System.out.println("Strings are not equal");
+        }
+    }
+}
